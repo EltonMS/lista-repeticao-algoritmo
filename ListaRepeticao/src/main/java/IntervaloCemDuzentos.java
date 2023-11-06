ee58de90ad44e7d75a8330d886dadc8a31b17a98
Elton Moreira <eltonms17@gmail.com>
2023-11-06 16:13:20 -0300

    1° Commit da Questão 3
    1° versão da questão 3, falta melhorar o tipo de laço.

---
+++ ListaRepeticao/src/main/java/IntervaloCemDuzentos.java
@@ -0,0 +1,29 @@
+
+/**
+ *
+ * @author Elton
+ */
+//Ler vários números e informar quantos números entre 100 e 200 foram digitados. Quando o 
+//valor zero for lido, o algoritmo deverá cessar sua execução.
+import java.util.Scanner;
+
+public class IntervaloCemDuzentos {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i;
+        int numeros_entre_cem_duzentos = 0;
+  //Preciso mudar o tipo de laço para ler quantos numeros quiser posteriormente(while), e parar apenas com o 0 digitado.
+        for (i = 1; i < 5; i++) {
+            System.out.println("Digite um Numero: ");
+            int numero_digitado = scanner.nextInt();
+            if (numero_digitado >= 100 && numero_digitado <= 200) {
+                numeros_entre_cem_duzentos++;
+            }
+            if (numero_digitado == 0) {
+                break;
+            }
+        }
+        System.out.println("O total de numeros entre 100 e 200 é: " + numeros_entre_cem_duzentos);
+    }
+}
